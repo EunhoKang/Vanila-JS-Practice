af0bec3e71f56cbecf631bab907d6466e907f78b
EunhoKang <1999paul@naver.com>
2021-04-24 16:30:52 +0900

    210424

---
+++ chat/app.js
@@ -0,0 +1,22 @@
+const express=require("express");   //require로 모듈을 가져온다. 
+const app=express();                //express 객체 생성
+const path=require("path");
+const http=require("http");
+const server=http.createServer(app);//서버 생성.
+const socketIO=require("socket.io");
+const io=socketIO(server);          //
+
+app.use(express.static(path.join(__dirname,"src"))); 
+//"scr"라는 이름의 디렉토리에 포함된 이미지, CSS 파일 및 JavaScript 파일을 제공
+//path.join으로 현재 좌표와 scr라는 폴더의 경로를 이어붙인다. 운영체제마다 이어붙이는 방식이 다르기 때문.
+const PORT=process.env.PORT || 5000; //프로세스 환경에 포트가 지정되어 있으면 그것을 사용하고 아니면 5000번 사용
+
+io.on('connection',(socket)=>{
+    socket.on("chatting",(data)=>{
+        console.log(data);
+        io.emit("chatting","from server");
+    });
+});
+
+server.listen(PORT,() => console.log(`server is running ${PORT}`));
+
